eddb1c5d9ba40c8fb4de8ded751804160481b288
Gea-Suan Lin <gslin@gslin.org>
2017-02-01 06:41:50 +0800

    Import.

---
+++ medium-hash-cleaner.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Medium Hash Cleaner
+// @namespace    https://blog.gslin.org/medium-hash-cleaner/
+// @version      0.2
+// @description  Medium hash cleaner
+// @author       Gea-Suan Lin
+// @match        *://*/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var mediumSite = false;
+    var ss = document.getElementsByTagName('script');
+    var len = ss.length;
+    for (var i = 0; i < len; i++) {
+        var url = ss[i].src;
+        if (undefined !== url && -1 !== url.indexOf('://cdn-static-1.medium.com/_/fp/gen-js/')) {
+            mediumSite = true;
+            break;
+        }
+    }
+
+    if (mediumSite) {
+        window.history.replaceState = function(){};
+
+        if ('' !== window.location.hash) {
+            window.location = '';
+        }
+    }
+})();
